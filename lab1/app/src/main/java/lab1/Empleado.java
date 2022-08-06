391a3444ec3387a4e3b48251f009440f9bcf8eb5
Elias Gill <eliasgill42@gmail.com>
2022-08-06 10:15:27 -0400

    mas dolor

---
+++ lab1/app/src/main/java/lab1/Empleado.java
@@ -0,0 +1,27 @@
+package lab1;
+
+public class Empleado {
+    String nombre;
+    String apellido;
+    double salario;
+
+    // setters y getters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+}
