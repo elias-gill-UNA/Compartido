af2772041c80318dbc8b1f987a39f46edf18f918
androceo123 <105460729+androceo123@users.noreply.github.com>
2022-08-14 16:38:54 -0400

    ejercicio 1 lab 2
    aca esta lo que hice, por fa vean si es todo lo que pide plox

---
+++ ejercicio 1 lab 2/lab2/src/lab2/vehiculo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Andres Moises
+ */
+public class vehiculo extends maquina implements accionesAuto{
+    private int ruedas;
+    public void arrancar(){
+        System.out.println("Has arrancado el vehiculo");
+    }
+    
+    public void frenar(){
+        System.out.println("has frenado el vehiculo");
+    }
+    
+    public void parar(){
+        System.out.println("has parado el vehiculo");
+    }
+    
+    public void acelerar(){
+        System.out.println("has acelerado el vehiculo");
+    }
+    
+    public void sonarVocina(){
+        System.out.println("PI PI PIIIIII");
+    }
+    public void cantidadRuedas(){
+        System.out.println("no se sabe la cantidad");
+    }
+}
