7d16fc2cbb407aa951b7f2632d923ea28bf45c40
Elias Gill <eliasgill42@gmail.com>
2022-08-16 12:37:48 -0400

    terminado terminadisimo

---
+++ lab2/ejercicio3/src/ejercicio3/PruebaEmpleado.java
@@ -0,0 +1,11 @@
+package ejercicio3;
+
+// @author AbelDíaz
+public class PruebaEmpleado {
+
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado("Abel", "Díaz", 5404806);
+        System.out.println(empleado.toString());
+    }
+
+}
---
+++ lab2/ejercicio3/src/ejercicio3/PruebaEmpleadoBaseMasComision.java
@@ -0,0 +1,10 @@
+package ejercicio3;
+// @author AbelDíaz
+
+public class PruebaEmpleadoBaseMasComision {
+
+    public static void main(String[] args) {
+        EmpleadoBaseMasComision empleado = new EmpleadoBaseMasComision("Abel", "Díaz", 5404806, 500000, 0.05, 3000000);
+        System.out.println(empleado.toString());
+    }
+}
