8d4fda67bde40c75f1e32ee2cc8cfd1ccf32947b
abeldiaz17 <diazabel743@gmail.com>
2022-08-16 11:26:23 -0400

    Ejercicio 3 y 4 listo

---
+++ lab2/ejercicio4/src/ejercicio4/Empleado.java
@@ -0,0 +1,34 @@
+package ejercicio4;
+
+// @author AbelDíaz
+//  Superclase Empleado
+class Empleado {
+
+    private final String primerNombre;
+    private final String apellidoPaterno;
+    private final int numeroCedula;
+
+    Empleado(String primerNombre, String apellidoPaterno, int numeroCedula) {
+        this.primerNombre = primerNombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.numeroCedula = numeroCedula;
+    }
+
+    public String obtenerPrimerNombre() {
+        return primerNombre;
+    }
+
+    public String obtenerApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public int obtenerNumeroCedula() {
+        return numeroCedula;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + obtenerPrimerNombre() + "\nApellido: " + obtenerApellidoPaterno()
+                + "\nNumero de cedula: " + obtenerNumeroCedula();
+    }
+}
