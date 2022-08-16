ccd6ae35c51e9300394b665735b87831e941482b
Elias Gill <eliasgill42@gmail.com>
2022-08-15 21:06:39 -0400

    ejercicio2

--- lab2/ejercicio2/src/Main/PruebaEmpleadoPorComision.java
+++ lab2/ejercicio2/src/Main/PruebaEmpleadoPorComision.java
@@ -1,6 +1,25 @@
-package  Main;
+package Main;
 
 public class PruebaEmpleadoPorComision {
     public static void main(String[] args) {
+        EmpleadoPorComision empleado = new EmpleadoPorComision("Elias", "Gill", "123456", 1100000, 0.25);
+
+        // datos originales
+        System.out.printf(
+                "Datos del empleado \nNombre y apellido: %s %s \nCedula: %s \nVentas: %s \nComision: \\% %double (%double)",
+                empleado.obtenerPrimerNombre(), empleado.obtenerApellidoPaterno(), empleado.obtenerVentasBrutas(),
+                empleado.ingresos(), empleado.obtenerTarifaComision());
+
+        empleado.establecerVentasBrutas(2000);
+        empleado.establecerTarifaComision(0.1);
+        // nuevos datos
+        System.out.printf(
+                "Datos del empleado \nNombre y apellido: %s %s \nCedula: %s \nVentas: %s \nComision: \\% %double (%double)",
+                empleado.obtenerPrimerNombre(), empleado.obtenerApellidoPaterno(), empleado.obtenerVentasBrutas(),
+                empleado.ingresos(), empleado.obtenerTarifaComision());
+
+        // metodo toString
+        System.out.println("==================\nMetodo toString()");
+        System.out.println(empleado.toString());
     }
 }
