caca2acf59bd2adffe04c2c2d1c157705ea67806
Gaussdrigo <alvarengar49@gmail.com>
2022-09-26 21:45:05 -0400

    ahora si

--- lab5/ejercicio4/trasaccion/src/trasaccion/Trasaccion.java
+++ lab5/ejercicio4/trasaccion/src/trasaccion/Trasaccion.java
@@ -17,6 +17,10 @@ public class Trasaccion {
     public static void main(String[] args) {
         costo cliente=new costo(456789,10,5,120000);
         cliente.costo_trasanccion();
+        costo cliente2=new costo(456,9,12,2400000);
+        cliente2.costo_trasanccion();
+        costo client3=new costo(987789,8,6,980000);
+        client3.costo_trasanccion();
     }
     
 }
