c23a226a8232e65f935511636804147660a08d71
androceo123 <105460729+androceo123@users.noreply.github.com>
2022-10-03 09:28:47 -0300

    ejercicio1 andres
    ojala no genere conflicto

---
+++ lab6/lab6.1/src/lab6/pkg1/Lab61.java
@@ -0,0 +1,43 @@
+package lab6.pkg1;
+import java.util.Scanner;
+
+public class Lab61 {
+
+    public static void main(String[] args) {
+        
+        int[] arreglo={0,0,0,0};/*arreglo con tamaño 4, porque 4 numeros son los que son necesarios almacenar, no 5, pq pueden ser 4 
+        numero distintos*/
+        int numero=0;
+        int contador=0;
+        
+        while(contador<5){//se le pide al usuario ingresar 5 numeros siempre
+            
+            while(numero<10 || numero>100){//si no esta entre el rango solicitado
+                System.out.println("ingrese un valor entre 10 y 100");
+                Scanner eleccion = new Scanner(System.in);
+                numero = eleccion.nextInt();
+                System.out.println("");
+            }
+            
+            if(encontrarDuplicado(arreglo,numero)==0){//si no es duplicado, lo imprimimos
+                System.out.println("El numero es: "+numero);
+                System.out.println("");
+            }
+            if(contador!=4){//porque no es necesario seguir cargando valores en el arreglo de duplicados
+                arreglo[contador]=numero;
+            }
+            contador++;//para finalizar la bandera
+            numero=0;//para que entre en el while de la linea 12
+        }
+    }
+    
+    static int encontrarDuplicado(int[] arreglo,int elemento){//funcion que detecta si hay un numero repetido
+        int bandera=0;//bandera que se activa si es un numero duplicado
+        for(int i=0;i<arreglo.length;i++){//recorre el arreglo hasta encontrar el valor duplicado
+            if(elemento==arreglo[i]){//si encuntra un valor duplicado, ejecuta la linea de abajo
+                bandera=1;//si encuntra un valor duplicado activa la bandera
+            }
+        }
+        return bandera;//retorna la bandera
+    }
+}
