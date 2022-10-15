050e7526f312e4a2c7a6a4dbc15caee57f77e8d7
Elias Gill <eliasgill42@gmail.com>
2022-10-15 01:03:45 -0300

    anadido un linter con github actions

--- lab7/ejercicio2/src/lab7/pkg2/Lab72.java
+++ lab7/ejercicio2/src/lab7/pkg2/Lab72.java
@@ -2,28 +2,33 @@ package lab7.pkg2;
 
 public class Lab72 {
     public static void main(String[] args) {
-        figura[] papa = new figura[6];//arreglo de las figuras trigonometricas(alberga todo tipo de objetos)
-        cuadrado cuadradito = new cuadrado("cuadradito",5);//objeto cuadrado
-        triangulo triangulito = new triangulo("triangulito",5,6);//objeto triangulo
-        circulo circulito = new circulo("circulito",5);//objeto circulo
-        cubo cubito = new cubo("cubito",5);//objeto cubo
-        tetraedro tetraedrito = new tetraedro("tetraedrito",5);//objeto tetraedro
-        esfera esferita = new esfera("esferita",5);//objeto esfera
-        papa[0]=cuadradito;
-        papa[1]=triangulito;
-        papa[2]=circulito;
-        papa[3]=cubito;
-        papa[4]=tetraedrito;
-        papa[5]=esferita;
-        for(int i=0;i<6;i++){
+        // arreglo de las figuras trigonometricas(alberga todo tipo de objetos)
+        figura[] papa = new figura[6];
+        // figuras bidimensionales
+        cuadrado cuadradito = new cuadrado("cuadradito", 5); // objeto cuadrado
+        triangulo triangulito = new triangulo("triangulito", 5, 6); // objeto triangulo
+        circulo circulito = new circulo("circulito", 5); // objeto circulo
+        // figuras tridimensionales
+        cubo cubito = new cubo("cubito", 5); // objeto cubo
+        tetraedro tetraedrito = new tetraedro("tetraedrito", 5); // objeto tetraedro
+        esfera esferita = new esfera("esferita", 5); // objeto esfera
+
+        papa[0] = cuadradito;
+        papa[1] = triangulito;
+        papa[2] = circulito;
+        papa[3] = cubito;
+        papa[4] = tetraedrito;
+        papa[5] = esferita;
+
+        for (int i = 0; i < 6; i++) {
             papa[i].mostrarDescripcion();
-            if(papa[i].tipo==2){//si es un objeto de tipo "figura bidimensional"
-                System.out.println("Mi area es: "+papa[i].obtenerArea());
-            }else{//si es un objeto de tipo "figura tridimensional"
-                System.out.println("Mi area es: "+papa[i].obtenerArea());
-                System.out.println("Mi volumen es: "+papa[i].obtenerVolumen());
+            if (papa[i].tipo == 2) {// si es un objeto de tipo "figura bidimensional"
+                System.out.println("Mi area es: " + papa[i].obtenerArea());
+            } else {// si es un objeto de tipo "figura tridimensional"
+                System.out.println("Mi area es: " + papa[i].obtenerArea());
+                System.out.println("Mi volumen es: " + papa[i].obtenerVolumen());
             }
-            System.out.println("");//espacio para que salga mas bonito
+            System.out.println("");// espacio para que salga mas bonito
         }
-    }   
+    }
 }
--- lab7/ejercicio2/src/lab7/pkg2/tetraedro.java
+++ lab7/ejercicio2/src/lab7/pkg2/tetraedro.java
@@ -1,23 +1,23 @@
 
 package lab7.pkg2;
 
-public class tetraedro extends figuraTridimensional{
-    public tetraedro(String nombre,float lado){
-        this.nombre=nombre;
-        this.lado=lado;
+public class tetraedro extends figuraTridimensional {
+    public tetraedro(String nombre, float lado) {
+        this.nombre = nombre;
+        this.lado = lado;
     }
-    
-    float obtenerArea(){
-        float area=(lado*lado)*(float)java.lang.Math.sqrt(3);
+
+    float obtenerArea() {
+        float area = (lado * lado) * (float) java.lang.Math.sqrt(3);
         return area;
     }
-    
-    float obtenerVolumen(){
-        float volumen=(lado*lado*lado)*((float)java.lang.Math.sqrt(2)/12);
+
+    float obtenerVolumen() {
+        float volumen = (lado * lado * lado) * ((float) java.lang.Math.sqrt(2) / 12);
         return volumen;
     }
-    
-    void mostrarDescripcion(){
-        System.out.println("Soy el objeto "+this.nombre+" y soy un tetraedro");
+
+    void mostrarDescripcion() {
+        System.out.println("Soy el objeto " + this.nombre + " y soy un tetraedro");
     }
 }
