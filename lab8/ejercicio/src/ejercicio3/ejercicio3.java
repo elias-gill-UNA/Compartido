46d093b81b737ed0f8389bd2d4d2e6b8bfc46707
Elias Gill <eliasgill42@gmail.com>
2022-10-23 19:50:43 -0300

    cambiando cositas

--- lab8/ejercicio/src/ejercicio3/ejercicio3.java
+++ lab8/ejercicio/src/ejercicio3/ejercicio3.java
@@ -4,50 +4,11 @@ import javax.swing.*;
 // @Autor Elias Gill
 
 public class ejercicio3 extends plantilla implements test_interface {
-    private class costo {
-        public Float numero_cuenta, total_trasanciones_mes, limite_transacciones, monto_total;
-
-        public costo(
-                int numero_cuenta,
-                int total_trasanciones_mes,
-                int limite_transacciones,
-                int monto_total) {
-            this.numero_cuenta = new Float(numero_cuenta);
-            this.total_trasanciones_mes = new Float(total_trasanciones_mes);
-            this.limite_transacciones = new Float(limite_transacciones);
-            this.monto_total = new Float(monto_total);
-        }
-
-        public void costo_trasanccion() {
-            if (total_trasanciones_mes > limite_transacciones) {
-
-                System.out.print("Costo a pagar: ");
-                if ((total_trasanciones_mes - limite_transacciones) <= 5) {
-                    System.out.println(monto_total * 0.02);
-
-                } else {
-                    System.out.println(monto_total * 0.05);
-                }
-
-            } else {
-                System.out.println("No se ha excedido en el limite de transacciones");
-            }
-
-        }
-    }
-
-    // primero definen sus inputs
-    private costo cliente[] = new costo[] {
-            new costo(456789, 10, 5, 120000),
-            new costo(456, 9, 12, 2400000),
-            new costo(987789, 18, 6, 980000)
-    };
-
+    // inputs
     static JTextPane listaInputs[] = new JTextPane[] {
-            new JTextPane(), // input neovim
             new JTextPane(),
-            new JTextPane(), // input calc. dif
-            new JTextPane()// input calc. dif
+            new JTextPane(),
+            new JTextPane(),
     };
 
     // difinir el titulo del input
@@ -55,7 +16,6 @@ public class ejercicio3 extends plantilla implements test_interface {
             "Numero de cuenta",
             "Limite Transacciones",
             "Total de Transacciones",
-            "Monto total"
     };
     // panel de resultado (pueden usar a7 nomas)
     static JTextPane res = new JTextPane();
@@ -67,42 +27,25 @@ public class ejercicio3 extends plantilla implements test_interface {
     // modificar esta funcion a discrecion
     @Override
     public void calcular() {
-        int resultado = 0;
         // aca estiran los datos de sus inputs
-        Float input1 = Float.parseFloat(listaInputs[0].getText());
-        Float input2 = Float.parseFloat(listaInputs[1].getText());
-        Float input3 = Float.parseFloat(listaInputs[2].getText());
-        Float input4 = Float.parseFloat(listaInputs[3].getText());
-        for (costo c : cliente) {
-            if (input1 == c.numero_cuenta) {
-                if (c.total_trasanciones_mes > c.limite_transacciones) {
-                    resultado = logica(c.total_trasanciones_mes, c.limite_transacciones, c.monto_total);
-                    if (resultado == 1) {
-                        res.setText(
-                                "El cliente se excede en el limite y debe pagar" + " " + "" + (c.monto_total * 0.02));
-                    } else if (resultado == 2) {
-                        res.setText(
-                                "El cliente se excede en el limite y debe pagar" + " " + "" + (c.monto_total * 0.05));
-                    } else {
-                        res.setText("El Cliente NO se ha excedido en sus transacciones");
-                    }
-                }
-            }
-        }
+        Float limite = Float.parseFloat(listaInputs[1].getText());
+        Float transacciones = Float.parseFloat(listaInputs[2].getText());
 
+        if (limite < transacciones) {
+            logica(transacciones - limite);
+        } else {
+            res.setText("Transaccion gratuita");
+        }
     }
 
     // funcion donde se realiza la logica del calculo
-    public Integer logica(Float... parametros) {
-
-        if ((parametros[0] - parametros[1]) <= 5) {
-            System.out.println(parametros[3] * 0.02);
-            return 1;
-        } else if ((parametros[0] - parametros[1]) > 5) {
-            System.out.println(parametros[3] * 0.05);
-            return 2;
+    public void logica(Float... parametros) {
+        if (parametros[0] <= 5) {
+            Double r = parametros[0] * 0.02;
+            res.setText("Costo de transaccion: " + r.toString());
         } else {
-            return 3;
+            Double r = parametros[0] * 0.05;
+            res.setText("Costo de transaccion: " + r.toString());
         }
     }
 
