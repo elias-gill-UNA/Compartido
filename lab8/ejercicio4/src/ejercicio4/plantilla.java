9dffd61db6f44b7956d397fb517f5b0cd3418da3
Abel Díaz <diazabel743@gmail.com>
2022-10-22 23:22:17 -0300

    esta mas o menos

---
+++ lab8/ejercicio4/src/ejercicio4/plantilla.java
@@ -0,0 +1,87 @@
+package ejercicio4;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public abstract class plantilla extends JFrame implements ActionListener {
+
+    public JComponent[] listaInputs;
+    public String listaTitulos[];
+    public JComponent res;
+
+    // clase que crea la parte del contenido que es modificable
+    private class contenidoModificable extends JComponent {
+
+        public contenidoModificable(JComponent components[], String titulos[]) throws Exception {
+            // nueva lista de maximo 6 elementos y con 1 sola columna
+            setLayout(new GridLayout(6, 1));
+            if (components.length < titulos.length) {
+                throw new Exception(
+                        "NO estas proporcionando todos los datos necesarios. Existen titulos vacios o titulos sin input");
+            }
+            for (int i = 0; i < titulos.length; i++) {
+                add(new nuevoInput(components[i], titulos[i]));
+            }
+        }
+    }
+
+    // crea un nuevo input en base al texto y al componente que se le proporciona
+    private class nuevoInput extends JComponent {
+
+        public nuevoInput(JComponent comp, String titulo) {
+            setLayout(new GridLayout(1, 2));
+            add(new JLabel(titulo));
+            add(comp);
+        }
+
+        public nuevoInput(JComponent comp, JComponent titulo) {
+            setLayout(new GridLayout(1, 2));
+            add(titulo);
+            add(comp);
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        calcular();
+    }
+
+    abstract public void calcular();
+
+    // TextFieldFrame constructor adds JTextFields to JFrame
+    public plantilla(JComponent res, JComponent listaInputs[], String listaTitulos[]) throws Exception {
+
+        super("Calculadora");
+        setLayout(new BorderLayout());
+        this.listaTitulos = listaTitulos;
+        this.listaInputs = listaInputs;
+        this.res = res;
+
+        // boton de calcular
+        JButton button = new JButton("Calcular");
+        button.addActionListener(this);
+
+        // contenido modificable
+        add("Center", new contenidoModificable(listaInputs, listaTitulos));
+        add("South", new nuevoInput(button, res));
+
+        // defualt size and options
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450, 200);
+        setVisible(true);
+        setLocation(450, 200);
+
+        // Pone el icono de la ventana
+        ImageIcon icono = new ImageIcon("calculadora.png");
+        this.setIconImage(icono.getImage());
+    }
+
+    public plantilla() {
+    }
+}
---
+++ lab8/ejercicio4/src/ejercicio4/test_interface.java
@@ -0,0 +1,8 @@
+package ejercicio4;
+
+interface test_interface {
+
+    void calcular();
+
+    Float logica(Float... list);
+}
