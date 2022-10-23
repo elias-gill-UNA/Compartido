dbc32a40ce1d9277655544506949a87a8e46782d
Elias Gill <eliasgill42@gmail.com>
2022-10-23 19:22:45 -0300

    limpiando

--- lab8/ejercicio2/src/ejercicio3/Lab82.java
+++ lab8/ejercicio2/src/ejercicio3/Lab82.java
@@ -1,20 +1,22 @@
 package ejercicio3;
 
+import java.text.DecimalFormat;
+
 import javax.swing.*;
 // @Autor Elias Gill
 
 public class Lab82 extends plantilla implements test_interface {
 
     // primero definen sus inputs
-    static JTextPane listaInputs[] = new JTextPane[]{
-        new JTextPane(), // input neovim
-        new JTextPane() // input calc. dif
+    static JTextPane listaInputs[] = new JTextPane[] {
+            new JTextPane(), // input neovim
+            new JTextPane() // input calc. dif
     };
 
     // difinir el titulo del input
-    static String listaTitulos[] = new String[]{
-        "Poblacion mundial",
-        "Tasa de crecimiento anual"
+    static String listaTitulos[] = new String[] {
+            "Poblacion mundial",
+            "Tasa de crecimiento anual"
     };
     // panel de resultado (pueden usar a7 nomas)
     static JTextArea res = new JTextArea();
@@ -30,26 +32,33 @@ public class Lab82 extends plantilla implements test_interface {
         Float input1 = Float.parseFloat(listaInputs[0].getText()); // saca de neovim
         Float input2 = Float.parseFloat(listaInputs[1].getText()); // saca de calculo
         // actualizar el resultado
-        //res.setText(logica(input1,input2));
+        // res.setText(logica(input1,input2));
         logica(input1, input2);
     }
 
     // funcion donde se realiza la logica del calculo
     public void logica(Float... arreglo) {
-        //unir todo
+        // unir todo
         Float poblacion = arreglo[0];
+        DecimalFormat df = new DecimalFormat();
         String r = new String();
-        r += ("La poblacion mundial hoy es: " + poblacion);
+
+        r += ("La poblacion actual: " + df.format(poblacion));
         poblacion = (poblacion + (poblacion * arreglo[1]));
-        r += ("\nLa poblacion mundial dentro de 1 año aproximadamente sera: " + poblacion);
+
+        r += ("\nPrimer año: " + df.format(poblacion));
         poblacion = (poblacion + (poblacion * arreglo[1]));
-        r += ("\nLa poblacion mundial dentro de 2 años aproximadamente sera: " + poblacion);
+
+        r += ("\nSegundo año: " + df.format(poblacion));
         poblacion = (poblacion + (poblacion * arreglo[1]));
-        r += ("\nLa poblacion mundial dentro de 3 años aproximadamente sera: " + poblacion);
+
+        r += ("\nTercer año: " + df.format(poblacion));
         poblacion = (poblacion + (poblacion * arreglo[1]));
-        r += ("\nLa poblacion mundial dentro de 4 años aproximadamente sera: " + poblacion);
+
+        r += ("\nCuarto año: " + df.format(poblacion));
         poblacion = (poblacion + (poblacion * arreglo[1]));
-        r += ("\nLa poblacion mundial dentro de 5 años aproximadamente sera: " + poblacion);
+
+        r += ("\nQuinto año: " + df.format(poblacion));
         res.setText(r);
     }
 
--- lab8/ejercicio2/src/ejercicio3/test_interface.java
+++ lab8/ejercicio2/src/ejercicio3/test_interface.java
@@ -2,5 +2,6 @@ package ejercicio3;
 
 interface test_interface {
     void calcular();
-    void logica(Float...a);
+
+    void logica(Float... a);
 }
