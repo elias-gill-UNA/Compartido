f81e70b380e767fc90aece24ba0ace42e25194ef
Elias Gill <eliasgill42@gmail.com>
2022-10-23 20:01:44 -0300

    cambiando cositas

--- lab8/ejercicio/src/ejercicio3/ejercicio3.java
+++ lab8/ejercicio/src/ejercicio3/ejercicio3.java
@@ -4,57 +4,57 @@ import javax.swing.*;
 // @Autor Elias Gill
 
 public class ejercicio3 extends plantilla implements test_interface {
-    // inputs
-    static JTextPane listaInputs[] = new JTextPane[] {
-            new JTextPane(),
-            new JTextPane(),
-            new JTextPane(),
-    };
+	// inputs
 
-    // difinir el titulo del input
-    static String listaTitulos[] = new String[] {
-            "Numero de cuenta",
-            "Limite Transacciones",
-            "Total de Transacciones",
-    };
-    // panel de resultado (pueden usar a7 nomas)
-    static JTextPane res = new JTextPane();
+	static JTextPane listaInputs[] = new JTextPane[]{
+		new JTextPane(),
+		new JTextPane(),
+		new JTextPane(),};
 
-    public ejercicio3() throws Exception {
-        super(res, listaInputs, listaTitulos);
-    }
+	// difinir el titulo del input
+	static String listaTitulos[] = new String[]{
+		"Monto de transaccion",
+		"Limite Transacciones",
+		"Total de Transacciones",};
+	// panel de resultado (pueden usar a7 nomas)
+	static JTextPane res = new JTextPane();
 
-    // modificar esta funcion a discrecion
-    @Override
-    public void calcular() {
-        // aca estiran los datos de sus inputs
-        Float limite = Float.parseFloat(listaInputs[1].getText());
-        Float transacciones = Float.parseFloat(listaInputs[2].getText());
+	public ejercicio3() throws Exception {
+		super(res, listaInputs, listaTitulos);
+	}
 
-        if (limite < transacciones) {
-            logica(transacciones - limite);
-        } else {
-            res.setText("Transaccion gratuita");
-        }
-    }
+	// modificar esta funcion a discrecion
+	@Override
+	public void calcular() {
+		// aca estiran los datos de sus inputs
+		Float monto = Float.parseFloat(listaInputs[0].getText());
+		Float limite = Float.parseFloat(listaInputs[1].getText());
+		Float transacciones = Float.parseFloat(listaInputs[2].getText());
 
-    // funcion donde se realiza la logica del calculo
-    public void logica(Float... parametros) {
-        if (parametros[0] <= 5) {
-            Double r = parametros[0] * 0.02;
-            res.setText("Costo de transaccion: " + r.toString());
-        } else {
-            Double r = parametros[0] * 0.05;
-            res.setText("Costo de transaccion: " + r.toString());
-        }
-    }
+		if (limite < transacciones) {
+			logica(transacciones - limite, monto);
+		} else {
+			res.setText("Transaccion gratuita");
+		}
+	}
 
-    public static void main(String[] args) {
-        try {
-            res.setEditable(false);
-            new ejercicio3();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	// funcion donde se realiza la logica del calculo
+	public void logica(Float... parametros) {
+		if (parametros[0] <= 5) {
+			Double r = parametros[1] * 0.02;
+			res.setText("Costo de transaccion: " + r.toString());
+		} else {
+			Double r = parametros[1] * 0.05;
+			res.setText("Costo de transaccion: " + r.toString());
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			res.setEditable(false);
+			new ejercicio3();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
